d57acbf2c943d122b407393a46371411cabcf986
luofuchang <yatou1394933257@gmail.com>
2025-05-12 14:49:51 +0800

    refactor(layout): 重构页面布局并简化首页组件
    - 引入 AppHeader 和 AppFooter 组件，替换原有的 header 和 footer
    - 移除 Home 组件中的高分榜相关代码，简化为只显示"Home"标题

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import React, { memo } from "react";
 import { Outlet } from "react-router-dom";
+import AppHeader from "./components/app-header";
+import AppFooter from "./components/app-footer";
 
 const App = memo(() => {
   return (
     <div className="app">
-      <div className="header">header</div>
+      <AppHeader />
       <div className="page">
         <Outlet />
       </div>
-      <div className="footer">footer</div>
+      <AppFooter />
     </div>
   );
 });
--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -1,25 +1,9 @@
-import React, { memo, useEffect, useState } from "react";
-import CustomRequest from "@/services";
+import React, { memo } from "react";
 
 const Home = memo(() => {
-  useEffect(() => {
-    CustomRequest.get({ url: "/home/highscore" }).then((res) => {
-      console.log(res);
-      setHighScore(res);
-    });
-  }, []);
-
-  const [highScore, setHighScore] = useState({});
-
   return (
     <div>
-      <h2>{highScore.title}</h2>
-      <h4>{highScore.subtitle}</h4>
-      <ul>
-        {highScore.list?.map((item) => {
-          return <li key={item.id}>{item.name}</li>;
-        })}
-      </ul>
+      <h2>Home</h2>
     </div>
   );
 });
